6fc0de3947bf30af4adf65b36e191cd2ec0e8270
idelgador <idelgador@choucairtesting.com>
2022-02-16 16:19:29 -0500

    Commit Inicial Proyecto Base

---
+++ base/src/main/java/chl/per/apl/app/mantenimiento/test/userinterface/ThePage.java
@@ -0,0 +1,8 @@
+package chl.per.apl.app.mantenimiento.test.userinterface;
+
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("https://www.google.com/")
+public class ThePage extends PageObject{
+}
